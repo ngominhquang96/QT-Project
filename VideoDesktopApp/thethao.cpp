fbf4356a728e921db8352129b3538f322c9e4fa2
ngominhquang96 <32742113+ngominhquang96@users.noreply.github.com>
2018-04-11 00:52:54 +0700

    Add files via upload

--- VideoDesktopApp/thethao.cpp
+++ VideoDesktopApp/thethao.cpp
@@ -2,35 +2,19 @@
 
 #include <QDir>
 #include <QDebug>
-#include <dirent.h>
 #include <QString>
+#include<QDirIterator>
+#include<QStringListModel>// duoc ke thua tu QAbstractListModel nen co the su dung
 
 TheThao::TheThao(QObject *parent):QThread(parent)
 {
 
 }
 void TheThao:: run(){
-    qDebug() <<"da test den day"<<"\n";
-    struct dirent *entry;
-    DIR *dir;
-    dir = opendir("G:/Video/thethao");
-    QString path="G:/Video/thethao";
-    if(!dir){
-        qDebug() << "Folder Empty" ;
-    }else{
-        qDebug() <<"da test den day 2"<<"\n";
-       entry=readdir(dir);
-       int i=0;
-        while (entry !=NULL) {
-            if(entry->d_name[0]!='.'){
-                QString filename=path.append("/").append(entry->d_name);
-                qDebug() <<"Da test den day"<<"\n";
-            }else{
-                qDebug() <<"Ko test vào được"<< i<<"\n";
-            }
-            i++;
-        }
-        qDebug() <<"Da ra ngoai vong while"<<"\n";
+    QStringListModel list= new QStringListModel();
+    QDirIterator it("G:/Video/thethao", QDirIterator::Subdirectories);
+    while (it.hasNext()) {
+        qDebug() << it.next();
     }
     for(int i = 0 ; i <10000; i++){
         qDebug() << "thethao" <<i<<"\n";
